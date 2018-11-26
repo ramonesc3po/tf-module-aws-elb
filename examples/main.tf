38d26117f5fcbb6be0a180e6770e0cfec972fc24
Ramon <rsalado7729@gmail.com>
2018-11-26 19:12:06 -0200

    Add examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,12 +1,71 @@
-data "" "" {}
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["*zigzaga*"]
+  }
 
-provider "terraform" {}
+  filter {
+    name   = "tag:Terraform"
+    values = ["true"]
+  }
+}
+
+data "aws_subnet" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"]
+  }
+
+  filter {
+    name   = "tag:Terraform"
+    values = ["true"]
+  }
+}
+
+resource "aws_security_group" "alb_java" {
+  name        = "alb"
+  description = "Conexao da Internet para o ALB"
+  vpc_id      = "${data.aws_vpc.selected.id}"
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      "name",
+    ]
+  }
+}
 
 module "alb" {
   source = "../"
 
-  organization = "zigzaga"
-  security_groups = "[]"
+  organization    = "zigzaga"
+  security_groups = "[${aws_security_group.alb_java.id}]"
+  subnets         = "[${data.aws_subnet.selected.*.id}]"
 
-  vpc_id = ""
+  vpc_id = "${data.aws_vpc.selected.id}"
+
+  tags = {
+    "Name"         = "zigzagaelb"
+    "Terraform"    = "true"
+    "Organization" = "zigzaga"
+  }
+
+  lb_timeouts = {
+    create = "20m"
+    update = "20m"
+    delete = "20m"
+  }
 }
