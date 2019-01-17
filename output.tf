db3abc6898ac38fe4d94ec45bdf0fb07e8408086
Ramon <rsalado7729@gmail.com>
2019-01-17 16:32:46 -0200

    Ajuste output

--- output.tf
+++ output.tf
@@ -42,7 +42,7 @@ output "tg_arn_suffix" {
 }
 
 output "lb_http_listener" {
-  value = "${element(concat(aws_lb_listener.http_no_logs.*.arn), 0)}"
+  value = "${element(concat(aws_lb_listener.http_no_logs.*.arn, list("")), 0)}"
 }
 
 output "lb_https_listener" {
